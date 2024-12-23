e5f83d7bedbd9cf6f1b61064513edf3de292be63
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2024-12-23 11:42:50 -0300

    ver02

---
+++ books/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers  # type: ignore
+from books.models import Books
+
+
+class BooksSerializer(serializers.ModelSeriaLizer):
+    class Meta:
+        model = Books
+        fields = "__all__"
+
+
+# O código < fields = "__all" > seria o ato de, o atributo fields receber todos
+# os atributos da classe Models, caso o programador queira utilizar somente um
+# atributos, basta passar em forma de string, o nome deste atributo, se for utilizado
+# dois atributos ou mais, deve ser passardo uma lista ou tupla de strings com os valores
+# dos atributos a serem utilizados.
+
+# O codigo "Type: ignore" foi solicitado pelo compilador para eliminar o aviso de erro
+# na importação do 'rest_framework'. Foram realizados testes de inicialização do servidor
+# e tudo ocorreu bem, manteremos atenção neste trecho de código.
--- books/models.py
+++ books/models.py
@@ -16,3 +16,22 @@ class Books(models.Model):
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
+
+
+"""
+        
+    id_book utilizada o método 'UUIDField', este método é responsável por 
+gerar automáticamente uma Id ao livro, para cada objeto criado pelo usuário
+
+    Em seus parâmetros, temos inicialmente o 'primay_key', este está sendo
+definido como verdadeiro para que esta ID seja armazenada no banco de dados
+como chave primária
+
+    Default=uuid4() é a padronização o tipo de id que será gerada durante a
+criação do objetgo.
+
+    Editable sendo verdadeira, proibe que esta chave gerada seja editada,
+tornando esta única e imutável.
+
+        
+"""
